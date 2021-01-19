5f88b404f4a2c162e45a8c0ac277fcca91e0426c
Wes Lawler <weslawler@Wess-MBP.attlocal.net>
2021-01-19 17:23:55 -0500

    adding navigation in modifying module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { Routes, RouterModule } from '@angular/router'
+//import { AppRoutingModule } from './app-routing.module.tx';
 import { AppComponent } from './app.component';
 import { ContainerComponentComponent } from './container-component/container-component.component';
-import { NavBarComponent } from './container-component/nav-bar/nav-bar.component';
+//import { NavBarComponent } from './container-component/nav-bar/nav-bar.component';
 import { BioComponent } from './container-component/bio/bio.component';
 import { HiringdesiresComponent } from './container-component/hiring-desires/hiring-desires.component';
+
+const routes: Routes = [
+  {
+    
+  }
+]
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ContainerComponentComponent,
-    NavBarComponent,
+   // NavBarComponent,
     BioComponent,
     HiringdesiresComponent,
     
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    //AppRoutingModule
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
