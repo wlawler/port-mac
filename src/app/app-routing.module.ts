be4d2969df200f91fd40ef3f4ec87708e638ca9c
Wes Lawler <weslawler@Wess-MBP.attlocal.net>
2021-02-02 17:48:01 -0500

    made first working nav bar on my own with currently the bio link working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {BioComponent} from './container-component/bio/bio.component';
 
 
 const routes: Routes = [
- 
+{
+  path: 'bio', 
+  component: BioComponent, 
+} 
 ];
 
 @NgModule({
